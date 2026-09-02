657b63f78c9bc7560e5b1104b59470a850cbd6ed
MalakaRM <160582947+MalakaRM@users.noreply.github.com>
2026-09-02 19:08:51 +0530

    update code

--- scripts/firebaseConfig.js
+++ scripts/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB5SEyMtqCyOy2Sv9RzpkK4GD6uY-8IPYY",
-  authDomain: "pos-system-c4f03.firebaseapp.com",
-  projectId: "pos-system-c4f03",
-  storageBucket: "pos-system-c4f03.firebasestorage.app",
-  messagingSenderId: "971625226594",
-  appId: "1:971625226594:web:3794817f44ed83848c8c3b",
-  measurementId: "G-SRXVZMBP76"
+  apiKey: "..........................",
+  authDomain: "........................",
+  projectId: "..............",
+  storageBucket: ".................",
+  messagingSenderId: ".................",
+  appId: "...........................",
+  measurementId: "...................."
 };
 
 firebase.initializeApp(firebaseConfig);
